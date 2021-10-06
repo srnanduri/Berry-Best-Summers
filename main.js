a6a7e002f4c642793a9feaa2520d1b3349686589
srnanduri <srnanduri@gmail.com>
2021-10-05 19:37:10 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function moreInfo() {
+    window.alert("When you email us, please give us your name and age, so that we give you appropriate replies. Please give us your email so we can mail you if we have anything to say. I hope you like our site!")
+}
+function funfact1() {
+    window.alert("Did you know that ultra-massive black holes are several black holes merged together? It has the most gravity!")
+}
+function funfact2() {
+    window.alert("Did you know that if planets replaced the moon, Mercury would probably be the safest planet to replace the moon with? It's 1.5 times bigger than the moon, so its impact on the tides wouldn't be harmful. Plus, it'll look just like the moon, only a tad bit bigger. But we're still happy with the good old moon!")
+}
+function funfact3() {
+    window.alert("Did you know that there is such a thing called a White Hole? It's the completely opposite of a black hole! It's alternate color, and the way it destroys! It expels the energy. But it lasts a shorter amount of time than the black hole. And if you go dangerously close to it, it won't harm you.")
+}
+function funfact4() {
+    window.alert("Last but not least, did you know that a contact binary star is two stars connected? It's true! Click on the link to see an image of it.")
+}
